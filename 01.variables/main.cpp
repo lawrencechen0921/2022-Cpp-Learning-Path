a4c99385bac870a37757fe910771e3ac6fe23217
lawrencechen0921 <show99520show99520@gmail.com>
2022-02-22 22:33:25 +0800

    Feature/Add folder

---
+++ 01.variables/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    float 稅前年收入;
+    cout<<"請輸入你的稅前年收入";
+    cin >> 稅前年收入;
+
+    float 稅前月收入 = 稅前年收入 / 12;
+
+    
+    cout << "你的稅前月收入為 " << 稅前月收入;
+
+    system("pause>0");
+}
+
