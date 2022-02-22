ab89c472d62b49e115e461edd13c41b8cde91308
lawrencechen0921 <show99520show99520@gmail.com>
2022-02-22 22:40:50 +0800

    Feature/Record Day 1 learning

--- 01.variables/main.cpp
+++ 01.variables/main.cpp
@@ -10,7 +10,12 @@ int main (){
     float 稅前月收入 = 稅前年收入 / 12;
 
     
-    cout << "你的稅前月收入為 " << 稅前月收入;
+    cout << "你的稅前月收入為 " << 稅前月收入 << endl;
+
+    cout << "你在未來十年會賺多少錢 " << 稅前年收入 * 10;
+    
+    char character = 'a';
+    /* Day 1 */
 
     system("pause>0");
 }
